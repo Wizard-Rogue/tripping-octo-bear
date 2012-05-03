2a01d4c9ac290bdc1060cd70da54ea10ab9212b4
Joseph Barrozo <JABarrozO@gmail.com>
2012-05-03 11:08:18 +0800

    initial save

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrippingOctoBear::Application.config.secret_token = 'ac6d527a96248d53b569238411248b98203596ab378f87fd86213f17e58a27a5842321217bacfab347c97f22a5289e54755a4ee0047d84bbed9e6661fd33d1db'
