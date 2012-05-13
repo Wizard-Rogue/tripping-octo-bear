00afbbe8080832a78a1eb49ced6321181ed46e17
April Canlas <seemeseeyou0415@gmail.com>
2012-05-13 22:06:04 +0800

    still editing css

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,9 +2,8 @@ class Event < ActiveRecord::Base
   belongs_to :organization
 
   attr_accessible :address, :city, :country, :description, :latitude, 
-             :longitude, :name, :organization_id, :state
+             :longitude, :name, :organization_id, :state, :start, :end, :photo
 
   has_attached_file :photo
 
-  
 end
