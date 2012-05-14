741ff971cc54936dea1ff91fadf72f050204df85
Joseph Barrozo <JABarrozO@gmail.com>
2012-05-14 16:06:06 +0800

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120514030819) do
+ActiveRecord::Schema.define(:version => 20120514064022) do
 
   create_table "events", :force => true do |t|
     t.integer  "organization_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20120514030819) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "name"
+    t.string   "screen_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
