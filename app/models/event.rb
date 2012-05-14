cd0ccc93a2da54443295259c1f5d12cbd2407547
April Canlas <seemeseeyou0415@gmail.com>
2012-05-14 16:54:03 +0800

    got the upload thingy to work

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,8 +2,11 @@ class Event < ActiveRecord::Base
 	belongs_to :organization
 	has_many :participants
 	has_many :users, :through => :participants
-  
+
+
+  attr_accessible :address, :city, :country, :description, :latitude, 
+             :longitude, :name, :organization_id, :state, :start, :end, :photo
+
 	has_attached_file :photo
 
-attr_accessible :address, :city, :country, :description, :latitude, :longitude, :name, :organization_id, :state
 end
