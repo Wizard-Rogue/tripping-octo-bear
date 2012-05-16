cd9c06d9483a595985ae9ad5ea9c74ffceee1917
Joseph Barrozo <JABarrozO@gmail.com>
2012-05-16 13:49:44 +0800

    finally got f***in twitter to work

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ TrippingOctoBear::Application.routes.draw do
 
   resources :events
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
   devise_scope :user do
   get '/users/auth/:provider' => 'users/omniauth_callbacks#passthru'
 	end
