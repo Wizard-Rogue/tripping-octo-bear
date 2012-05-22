9b2e3bffb803816c318eb526c0bdc3661c03dfb7
April Canlas <seemeseeyou0415@gmail.com>
2012-05-22 15:46:43 +0800

    MAY FORMS N BITCHEZ!!!

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,7 +6,6 @@
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
