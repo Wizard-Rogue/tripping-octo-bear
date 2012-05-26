2a619b91dd45d25ee4e743c8d958cc11f72f52a5
April Canlas <seemeseeyou0415@gmail.com>
2012-05-26 21:07:30 +0800

    Reg controller and other stuff

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -21,4 +21,23 @@ class RegistrationsController < Devise::RegistrationsController
 			super
 		end
 	end
+
+	def update
+    self.resource = resource_class.to_adapter.get!(send(:"current_#{resource_name}").to_key)
+
+    if resource.update_with_password(params[resource_name]) || ![:facebook, :twitter].include? resource.source
+      if is_navigational_format?
+        if resource.respond_to?(:pending_reconfirmation?) && resource.pending_reconfirmation?
+          flash_key = :update_needs_confirmation
+        end
+        set_flash_message :notice, flash_key || :updated
+      end
+      sign_in resource_name, resource, :bypass => true
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
+    end
+  end
+
 end
