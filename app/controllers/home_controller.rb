eab98c5bb154debb0a8274a29ba5912d1377eedc
Joseph Barrozo <JABarrozO@gmail.com>
2012-05-28 21:37:06 +0800

    hope this works

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,17 @@
 class HomeController < ApplicationController
   def index
-  	@events = Event.all
-  	@org = User.where(:is_organization => true).first
+  	@apology = false
+  	@nouser = false
+  	if Event.first.nil?
+  		apology = true;
+  	else
+	  	@events = Event.all
+	  	@event = Event.find(1)
+	end
+	if User.first.nil? || User.where(:is_organization => true).first.nil?
+		nouser = true;
+	else
+		@org = User.where(:is_organization => true).first
+	end
   end
 end
